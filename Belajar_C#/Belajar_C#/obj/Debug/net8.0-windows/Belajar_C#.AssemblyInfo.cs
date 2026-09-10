a09ccda78731e9d754a8eb8d5ce47bc30e5599ae
Glaceon - Naovee <banyubirudemas@gmail.com>
2026-09-10 14:41:23 +0700

    Add My Website

--- Belajar_C#/Belajar_C#/obj/Debug/net8.0-windows/Belajar_C#.AssemblyInfo.cs
+++ Belajar_C#/Belajar_C#/obj/Debug/net8.0-windows/Belajar_C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Belajar_C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bceb60c76cd849bf4d033d5e9aeeb6bcb54f0b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c3a63f90bcbae7b779f1595dc2ffd88fbe02541")]
 [assembly: System.Reflection.AssemblyProductAttribute("Belajar_C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Belajar_C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
